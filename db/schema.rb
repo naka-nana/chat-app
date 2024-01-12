fbac21b5f17e2779c804c1ca492652e73932031a
naka-nana <harunanapanda@icloud.com>
2024-01-12 18:29:53 +0900

    room_usersテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_12_090151) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_12_092529) do
+  create_table "room_users", charset: "utf8mb4", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "rooms", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
